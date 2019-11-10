5dd469588841c230e7cc4c926d54900abbb2dd15
WEIHAOLEE <liweihaodereng@163.com>
2019-11-10 19:21:46 +0800

    implementation requirement:submit form to servlet.java

--- src/com/bgd/sky/servlet/Servlet.java
+++ src/com/bgd/sky/servlet/Servlet.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("从form表单获取的内容是：用户名：" + username + "密码：" + password);
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        doPost(request, response);
     }
 }
